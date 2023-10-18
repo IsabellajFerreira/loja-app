13b929dec282e45ad80d67496905857edb7f1e58
IsabellajFerreira <isabellajferreira@hotmail.com>
2023-10-18 11:19:53 -0300

    Criando rota de id

--- src/app/produtos/mosfet/[slug]/page.jsx
+++ src/app/produtos/mosfet/[slug]/page.jsx
@@ -1,7 +1,7 @@
 export default function ProdutoSlug({params}) {
     return (
       <div>
-        <h1>Produto ID</h1>
+        <h1>Produto slug</h1>
         <p>Slug: {params.slug}</p>
       </div>
     );
