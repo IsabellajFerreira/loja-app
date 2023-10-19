96cd7b46bc46a4534f53472e81073d8fd99aee39
IsabellajFerreira <isabellajferreira@hotmail.com>
2023-10-19 11:18:21 -0300

    Pagina 404 com imagem externa. Layou-root integrado.

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,8 +1,12 @@
-
+import Image from "next/image";
 export default function NotFound() {
     return (
       <div>
           <h1>OOPS! 404!</h1>
+          <figure>
+          <Image src="https://http.cat/images/404.jpg" alt="gatp no jornal" width={750} height={600}/>
+        
+          </figure>
           <p>Página não encontrada!</p>
       </div>
     )
---
+++ src/components/Rodape/Rodape.jsx
@@ -0,0 +1,12 @@
+
+export default function Rodape() {
+    return (
+      <footer>
+          <div>
+              <p>Desevolvido por <span>RM552329</span></p>
+              <p>NEXT.js - React.js - tailwind</p>
+        </div>
+      </footer>
+    )
+  }
+  
